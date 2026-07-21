f8a89c34b9f2748667e70d4c60aab9de8246dcdf
AlIslamiTravel <farisgadkarim123@gmail.com>
2026-07-21 10:59:42 +0300

    Delete admin.js

--- admin.js
+++
@@ -1,4 +0,0 @@
-import { db, auth } from './firebase-config.js';
-import { collection, addDoc, serverTimestamp, getDocs, query, orderBy } from "firebase/firestore";
-import { signInWithEmailAndPassword, signOut, onAuthStateChanged } from "firebase/auth";
-import { showToast } from './
